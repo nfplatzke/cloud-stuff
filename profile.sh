a428f789f9e9c0cb06d7d0d3c91f1e083db3b72c
nfplatzke <neufpas@gmail.com>
2018-01-09 09:53:56 -0500

    add support for mfa tokens
    Added support for environments where MFA is required. If "mfa_token" is specified in the credentials file with a valid arn then the script will generate an sts session to be used in place of the credentials in the file.

--- profile.sh
+++ profile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+  
 # a script to more easily switch between aws profiles on the cli
 # combine with bash_profile in this repository to make life less dumb
 
@@ -8,17 +8,33 @@ PROFILE=$1
 if [ -z $PROFILE ]; then
     echo "Error: a profile argument must be supplied"
 else
-    ACCESS=$(aws configure get aws_access_key_id --profile $PROFILE 2>/dev/null)
-    SECRET=$(aws configure get aws_secret_access_key --profile $PROFILE 2>/dev/null)
     REGION=$(aws configure get region --profile $PROFILE 2>/dev/null)
+    MFA_DEVICE=$(aws configure get mfa_device --profile $PROFILE 2>/dev/null)
 
-    if [ -z $ACCESS ] || [ -z $SECRET ] || [ -z $REGION ]; then
+    if [ -n $MFA_DEVICE ]; then
+        echo -n "MFA Token: "
+        read TOKEN_CODE
+        STS_SESSION=$(aws sts get-session-token --serial-number $MFA_DEVICE --token-code $TOKEN_CODE)
+        ACCESS=$(echo $STS_SESSION | jq -r '.Credentials.AccessKeyId')
+        SECRET=$(echo $STS_SESSION | jq -r '.Credentials.SecretAccessKey')
+        SESSION=$(echo $STS_SESSION | jq -r '.Credentials.SessionToken')
+    else
+        ACCESS=$(aws configure get aws_access_key_id --profile $PROFILE 2>/dev/null)
+        SECRET=$(aws configure get aws_secret_access_key --profile $PROFILE 2>/dev/null)
+    fi
+
+    if [ -z $ACCESS ] || [ -z $SECRET ]; then
         echo "Error: $PROFILE does not exist or is missing info in ~/.aws/credentials"
     else
         export AWS_ACCESS_KEY_ID=$ACCESS
         export AWS_SECRET_ACCESS_KEY=$SECRET
+
+        if [ -n $SESSION ]; then
+            export AWS_SESSION_TOKEN=$SESSION
+        fi
+
         export AWS_DEFAULT_REGION=$REGION
-	export AWS_PROFILE=$PROFILE
+        export AWS_PROFILE=$PROFILE
     fi
 fi
 
