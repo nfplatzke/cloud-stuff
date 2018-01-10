f292f10d03d21e8c4dc4ba0a64ba5efbdb0b3af3
nfplatzke <neufpas@gmail.com>
2018-01-10 14:06:37 -0500

    bug fix related to sessions expiring
    users were unable to refresh a session using this script because the baseline credentials were never reloaded when a session expires. This new version unsets all keys as a first step, then loads the keys for an environment, and finally overrides them with the session token if the mfa_device variable exists in the credentials file

--- profile.sh
+++ profile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 # a script to more easily switch between aws profiles on the cli
 # combine with bash_profile in this repository to make life less dumb
 
@@ -8,33 +8,33 @@ PROFILE=$1
 if [ -z $PROFILE ]; then
     echo "Error: a profile argument must be supplied"
 else
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
+    unset AWS_SESSION_TOKEN
+    unset AWS_DEFAULT_REGION
+    unset AWS_PROFILE
+
+    ACCESS=$(aws configure get aws_access_key_id --profile $PROFILE 2>/dev/null)
+    SECRET=$(aws configure get aws_secret_access_key --profile $PROFILE 2>/dev/null)
     REGION=$(aws configure get region --profile $PROFILE 2>/dev/null)
     MFA_DEVICE=$(aws configure get mfa_device --profile $PROFILE 2>/dev/null)
+
+    if [ -z $ACCESS ] || [ -z $SECRET ]; then
+        echo "Error: $PROFILE does not exist or is missing info in ~/.aws/credentials"
+        exit
+    else
+        export AWS_ACCESS_KEY_ID=$ACCESS
+        export AWS_SECRET_ACCESS_KEY=$SECRET
+        export AWS_DEFAULT_REGION=$REGION
+        export AWS_PROFILE=$PROFILE
+    fi
 
     if [ -n $MFA_DEVICE ]; then
         echo -n "MFA Token: "
         read TOKEN_CODE
         STS_SESSION=$(aws sts get-session-token --serial-number $MFA_DEVICE --token-code $TOKEN_CODE)
-        ACCESS=$(echo $STS_SESSION | jq -r '.Credentials.AccessKeyId')
-        SECRET=$(echo $STS_SESSION | jq -r '.Credentials.SecretAccessKey')
-        SESSION=$(echo $STS_SESSION | jq -r '.Credentials.SessionToken')
-    else
-        ACCESS=$(aws configure get aws_access_key_id --profile $PROFILE 2>/dev/null)
-        SECRET=$(aws configure get aws_secret_access_key --profile $PROFILE 2>/dev/null)
-    fi
-
-    if [ -z $ACCESS ] || [ -z $SECRET ]; then
-        echo "Error: $PROFILE does not exist or is missing info in ~/.aws/credentials"
-    else
-        export AWS_ACCESS_KEY_ID=$ACCESS
-        export AWS_SECRET_ACCESS_KEY=$SECRET
-
-        if [ -n $SESSION ]; then
-            export AWS_SESSION_TOKEN=$SESSION
-        fi
-
-        export AWS_DEFAULT_REGION=$REGION
-        export AWS_PROFILE=$PROFILE
+        export AWS_ACCESS_KEY_ID=$(echo $STS_SESSION | jq -r '.Credentials.AccessKeyId')
+        export AWS_SECRET_ACCESS_KEY=$(echo $STS_SESSION | jq -r '.Credentials.SecretAccessKey')
+        export AWS_SESSION_TOKEN=$(echo $STS_SESSION | jq -r '.Credentials.SessionToken')
     fi
 fi
-
